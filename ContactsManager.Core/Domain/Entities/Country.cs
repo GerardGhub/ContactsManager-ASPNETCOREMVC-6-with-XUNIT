dc57a42a11df8641fc1b5024f034f639f7160227
Gerard Singian <singian28@gmail.com>
2024-07-03 02:14:18 +0800

    Model Comments

--- ContactsManager.Core/Domain/Entities/Country.cs
+++ ContactsManager.Core/Domain/Entities/Country.cs
@@ -2,16 +2,34 @@
 
 namespace Entities
 {
-  /// <summary>
-  /// Domain Model for Country
-  /// </summary>
-  public class Country
+    /// <summary>
+    /// Domain Model for Country
+    /// Represents a country entity with a unique identifier, name, and related persons.
+    /// </summary>
+    public class Country
   {
-    [Key]
+        /// <summary>
+        /// Gets or sets the unique identifier for the country.
+        /// The [Key] attribute indicates that this property is the primary key in the database.
+        /// </summary>
+
+
+        [Key]
     public Guid CountryID { get; set; }
 
-    public string? CountryName { get; set; }
+        /// <summary>
+        /// Gets or sets the name of the country.
+        /// This property is nullable, meaning the country might not have a name assigned.
+        /// </summary>
 
-    public virtual ICollection<Person>? Persons { get; set; }
+        public string? CountryName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of persons related to this country.
+        /// The 'virtual' keyword enables lazy loading of this collection if supported by the ORM (e.g., Entity Framework).
+        /// This property is nullable, meaning a country might not have any associated persons.
+        /// </summary>
+
+        public virtual ICollection<Person>? Persons { get; set; }
   }
 }
--- ContactsManager.Core/Domain/Entities/Person.cs
+++ ContactsManager.Core/Domain/Entities/Person.cs
@@ -1,46 +1,112 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Entities
 {
- /// <summary>
- /// Person domain model class
- /// </summary>
- public class Person
+    /// <summary>
+    /// Person domain model class
+    /// Represents a person entity with various attributes such as name, email, date of birth, etc.
+    /// </summary>
+    public class Person
  {
-  [Key]
+
+        /// <summary>
+        /// Gets or sets the unique identifier for the person.
+        /// The [Key] attribute indicates that this property is the primary key in the database.
+        /// </summary>
+
+        [Key]
   public Guid PersonID { get; set; }
 
-  [StringLength(40)] //nvarchar(40)
+        /// <summary>
+        /// Gets or sets the name of the person.
+        /// The [StringLength(40)] attribute limits the length of the name to 40 characters.
+        /// This property is nullable, indicating the person's name might not be set.
+        /// </summary>
+
+        [StringLength(40)] //nvarchar(40)
                      //[Required]
   public string? PersonName { get; set; }
 
-  [StringLength(40)] //nvarchar(40)
+
+        /// <summary>
+        /// Gets or sets the email of the person.
+        /// The [StringLength(40)] attribute limits the length of the email to 40 characters.
+        /// This property is nullable, indicating the person's email might not be set.
+        /// </summary>
+
+        [StringLength(40)] //nvarchar(40)
   public string? Email { get; set; }
 
-  public DateTime? DateOfBirth { get; set; }
 
-  [StringLength(10)] //nvarchar(100)
+        /// <summary>
+        /// Gets or sets the date of birth of the person.
+        /// This property is nullable, indicating the person's date of birth might not be set.
+        /// </summary>
+        public DateTime? DateOfBirth { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets the gender of the person.
+        /// The [StringLength(10)] attribute limits the length of the gender to 10 characters.
+        /// This property is nullable, indicating the person's gender might not be set.
+        /// </summary>
+
+        [StringLength(10)] //nvarchar(100)
   public string? Gender { get; set; }
 
-  //uniqueidentifier
-  public Guid? CountryID { get; set; }
 
-  [StringLength(200)] //nvarchar(200)
+        /// <summary>
+        /// Gets or sets the unique identifier for the country associated with the person.
+        /// This property is nullable, indicating the person's country might not be set.
+        /// </summary>
+
+        //uniqueidentifier
+        public Guid? CountryID { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets the address of the person.
+        /// The [StringLength(200)] attribute limits the length of the address to 200 characters.
+        /// This property is nullable, indicating the person's address might not be set.
+        /// </summary>
+
+        [StringLength(200)] //nvarchar(200)
   public string? Address { get; set; }
 
-  //bit
-  public bool ReceiveNewsLetters { get; set; }
 
-  //[Column("TaxIdentificationNumber", TypeName = "varchar(8)")]
-  public string? TIN { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether the person wants to receive newsletters.
+        /// The default value is false.
+        /// </summary>
+        //bit
+        public bool ReceiveNewsLetters { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tax identification number (TIN) of the person.
+        /// This property was previously configured to be stored as a varchar(8) in the database.
+        /// This property is nullable, indicating the person's TIN might not be set.
+        /// </summary>
+
+        //[Column("TaxIdentificationNumber", TypeName = "varchar(8)")]
+        public string? TIN { get; set; }
+
+        /// <summary>
+        /// Gets or sets the country associated with the person.
+        /// The [ForeignKey("CountryID")] attribute indicates that this property is a foreign key relationship.
+        /// This property is nullable, indicating the person might not have an associated country.
+        /// </summary>
 
 
-  [ForeignKey("CountryID")]
+        [ForeignKey("CountryID")]
   public virtual Country? Country { get; set; }
 
-  public override string ToString()
+        /// <summary>
+        /// Returns a string representation of the person entity.
+        /// </summary>
+        /// <returns>A string that represents the person entity.</returns>
+
+        public override string ToString()
   {
    return $"Person ID: {PersonID}, Person Name: {PersonName}, Email: {Email}, Date of Birth: {DateOfBirth?.ToString("MM/dd/yyyy")}, Gender: {Gender}, Country ID: {CountryID}, Country: {Country?.CountryName}, Address: {Address}, Receive News Letters: {ReceiveNewsLetters}";
   }
